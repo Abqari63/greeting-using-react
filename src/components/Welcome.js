fcc9b122661e06f7db950da7409a8c405adfb2b6
Abqari Abbas <82696169+Abqari63@users.noreply.github.com>
2024-03-09 14:32:45 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ export default function welcome(props) {
     return (
         <div>
             <div id="greet-msg">
-                <h1>Hey...! {props.name} Welcome...!</h1>
+                <h1>Hey...! {props.name}</h1>
                 <h2>Welcome to the college.</h2>
                 <p>Assignment done by Abqari Abbas ({props.reg})</p>
             </div>
